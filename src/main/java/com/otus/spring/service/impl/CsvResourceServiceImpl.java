4f149c67e021c149d16d216c725b3807051a5a8a
Andrei Bogomja <bogomja@gmail.com>
2018-08-07 00:37:56 +0300

    Replace ResourceService with Resource class attribute and use AppLocaleSettings to create a path for resource

--- src/main/java/com/otus/spring/service/impl/CsvResourceServiceImpl.java
+++ src/main/java/com/otus/spring/service/impl/CsvResourceServiceImpl.java
@@ -2,9 +2,11 @@ package com.otus.spring.service.impl;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import com.otus.domain.model.Quiz;
+import com.otus.spring.config.AppLocaleSettings;
 import com.otus.spring.service.CsvResourceService;
-import com.otus.spring.service.ResourceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
@@ -14,20 +16,18 @@ import java.io.InputStreamReader;
 import java.util.List;
 
 @Service
-public class CsvResourceServiceImpl implements CsvResourceService
-{
-    private final ResourceService resourceService;
+public class CsvResourceServiceImpl implements CsvResourceService {
+
+    private final Resource resource;
 
     @Autowired
-    public CsvResourceServiceImpl(final ResourceService resourceService)
-    {
-        this.resourceService = resourceService;
+    public CsvResourceServiceImpl(final AppLocaleSettings locale, final ResourceLoader resLoader) {
+        this.resource = resLoader.getResource(locale.getFile() + locale.getI18n() + locale.getExt());
     }
 
-    public List<Quiz> getQuizList()
-    {
+    public List<Quiz> getQuizList() {
         List<Quiz> beans = null;
-        try (InputStream is = resourceService.getCsvResource().getInputStream();
+        try (InputStream is = resource.getInputStream();
              BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
             beans = new CsvToBeanBuilder<Quiz>(br).withType(Quiz.class).build().parse();
         }
