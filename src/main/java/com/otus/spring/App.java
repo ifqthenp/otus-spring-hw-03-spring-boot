d67f5574fecea7a428a201499266591c6c5a840d
Andrei Bogomja <bogomja@gmail.com>
2019-01-09 22:02:13 +0200

    Disable 'script' command

--- src/main/java/com/otus/spring/App.java
+++ src/main/java/com/otus/spring/App.java
@@ -2,11 +2,14 @@ package com.otus.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        String[] disabledCommands = {"--spring.shell.command.script.enabled=false"};
+        String[] fullArgs = StringUtils.concatenateStringArrays(args, disabledCommands);
+        SpringApplication.run(App.class, fullArgs);
     }
 }
